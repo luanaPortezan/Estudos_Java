913ada1b33429387af139232fb475bfb809fcd44
Luana Portezan <luanaportezan@gmail.com>
2024-11-10 17:47:27 -0300

    vendo estruturas condicionais e estruturas de repetição(for)

--- src/com/betrybe/sistemadenota/SistemaDeNota.java
+++ src/com/betrybe/sistemadenota/SistemaDeNota.java
@@ -1,4 +1,30 @@
 package com.betrybe.sistemadenota;
 
+import
+
 public class SistemaDeNota {
+    public static void main(String[] args) {
+        double mediaFinal = 7.5;
+        String resultado =  "INDEFINIDO";
+
+        if (mediaFinal >= 7) {
+            resultado = "APROVADA";
+        } else if (mediaFinal >= 6.0) {
+            resultado = "REALIZAR PROVA BONUS";
+        } else {
+            resultado = "REPROVADA";
+        }
+
+        int numero;
+        String log;
+
+        // Leia como:
+        //   PARA numero = 1,
+        //   contanto que numero <= 10,
+        //   faça o loop e depois faça numero += 1
+        for (numero = 1; numero <= 10; numero += 1) {
+            log = "O Número atual é: " + numero + "\n";
+            System.out.println(log);
+        }
+    }
 }
