3d730a0e89c746b2854aef164472219f9824d9a9
Luana Portezan <luanaportezan@gmail.com>
2024-11-10 11:26:20 -0300

    primeiros passos em java

---
+++ src/com/betrybe/helloword/RaizQuadradaDeVinteECinco.java
@@ -0,0 +1,11 @@
+package com.betrybe.helloword;
+
+import java.lang.*;
+import java.lang.Math;
+
+public class RaizQuadradaDeVinteECinco {
+    public static void main(String[] args) {
+        System.out.println("Raiz quadrada de vinte e cinco é:");
+        System.out.println(Math.sqrt(25));
+    }
+}
