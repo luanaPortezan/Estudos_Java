5a9512fdbbe5b727680c01d65871d5779a58f432
Luana Portezan <luanaportezan@gmail.com>
2024-11-12 18:17:48 -0300

    comparando valores

---
+++ src/com/betrybe/helloword/ComparandoValores.java
@@ -0,0 +1,36 @@
+package com.betrybe.helloword;
+
+import java.util.Scanner;
+
+public class ComparandoValores {
+    public static void main(String[] args) {
+        //COMPRAÇÃO COM ==
+        int numero = 5; // Atribuição por valor.
+        System.out.println(numero == 5); // Imprime true.
+
+        //COMPARAÇÃO QUE PODE RECEBER VALORES NULL
+        Scanner scanner = new Scanner(System.in);
+
+        String hello = "Olá"; // Novo objeto do tipo String instanciado.
+        System.out.println("Diga Olá: ");
+        String input = scanner.next(); // Novo objeto do tipo String instanciado.
+
+// Objetos diferentes, mesmo que com o mesmo valor serão tratados como diferentes pelo comparador ==.
+        if (hello == input) {
+            System.out.println("Isso não será impresso.");
+        }
+
+        if (hello != input) {
+            System.out.println("Isso será impresso.");
+        }
+
+        if (hello == hello) {
+            System.out.println("Isso também será impresso, é o mesmo objeto.");
+        }
+
+// Verifica se o conteúdo armazenado em ambos os objetos é o mesmo através do método equals.
+        if (hello.equals(input)) {
+            System.out.println("Isso pode ser impresso se Olá for inserido em nosso terminal.");
+        }scanner.close();
+    }
+}
--- src/com/betrybe/sistemadenota/SistemaDeNota.java
+++
@@ -1,9 +0,0 @@
-package com.betrybe.sistemadenota;
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> parent of 913ada1 (vendo estruturas condicionais e estruturas de repetição(for))
-public class SistemaDeNota {
-}
